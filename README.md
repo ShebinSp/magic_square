cbb0a93ab45c29d21a3153fc6240316449ab893c
ShebinSp <shebinsp@gmail.com>
2024-04-22 21:10:14 +0530

    readme file updated

---
+++ README.md
@@ -0,0 +1,34 @@
+# Magic Square Checker
+
+## Description
+This program checks whether the given matrix is a magic square or not. A magic square is a square matrix where the sum of the numbers in each row, each column, and both main diagonals is the same. If the given matrix is a magic square, it prints "You have the Magic Square!", otherwise, it prints "Sorry.. its not a Magic Square, but its cool.. Please try again!".
+
+## Features
+- Checks whether the given matrix is a magic square or not.
+- User-friendly interface.
+- Written in C.
+
+## How to Use
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/ShebinSp/magic-square-checker.git
+ * To stop the program press `ctrl + c`
+
+## Example
+Enter the size of the matrix: 3
+Enter the elements in the matrix:
+1 1: 8
+1 2: 3
+1 3: 4
+2 1: 1
+2 2: 5
+2 3: 9
+3 1: 6
+3 2: 7
+3 3: 2
+       :-----------------------------:
+       |*                           *|
+       | !!!You got the MAGIC BOX!!! |
+       |*                           *|
+       :-----------------------------:
+
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+var i, j, n, ldsum, rdsum, magic int
+var ar [30][30]int
+var sor [10]int
+var soc [10]int
+var isExit = false
+
+func main() {
+
+	for !isExit {
+		magicSquare()
+	}
+}
+func magicSquare() {
+reset:
+	fmt.Print("\n-> Enter the size of the Magic Square (2 for 2x2 , 3 for 3x3): ")
+	fmt.Scanln(&n)
+	if n <= 1 {
+		fmt.Printf("\n! The Magic Square should be have minimum size of 2x2\n\n ")
+		goto reset
+	}
+	fmt.Println("-> Enter the elements in the matrix:")
+	for i = 0; i < n; i++ {
+		for j = 0; j < n; j++ {
+			fmt.Printf("%d %d: ", i+1, j+1)
+			fmt.Scanln(&ar[i][j])
+			soc[i], sor[i] = 0, 0
+		}
+	}
+	for i = 0; i < n; i++ {
+		for j = 0; j < n; j++ {
+			sor[i] += ar[i][j]
+			soc[i] += ar[j][i]
+			if i == j {
+				rdsum += ar[i][j]
+			}
+			if i+j == n-1 {
+				ldsum += ar[i][j]
+			}
+		}
+		if rdsum == sor[i] && ldsum == soc[i] {
+			magic++
+		}
+	}
+	if magic > 0 {
+		fmt.Printf("\n  yoo!!\n")
+		fmt.Println("       :---------------------------------:")
+		fmt.Println("       |*                               *|")
+		fmt.Println("       | !!!You have the MAGIC SQUARE!!! |")
+		fmt.Println("       |*                               *|")
+		fmt.Println("       :---------------------------------:")
+		fmt.Println()
+		isExit = true
+	} else {
+		fmt.Println("I am sorry.. it is a not a Magic Square, but thats cool..")
+		fmt.Println("                   Please try again!")
+		fmt.Printf("\n\n\n")
+	}
+}
